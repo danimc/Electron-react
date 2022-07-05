dfec7a71031a4dd02eaf3bfd57e3cb3cf07186e8
Danny Mc <daniel_k310a@hotmail.com>
2022-07-05 15:19:43 -0500

    instalaciones para build?

--- src/components/panelControl/index.js
+++ src/components/panelControl/index.js
@@ -3,16 +3,7 @@ import Video from "../video";
 import "./style.css"
 
 const SECURITY = [
-  'http://192.168.0.30:8000/live/stream.flv',
-  'http://192.168.0.30:8000/live/stream2.flv',
-  'http://192.168.0.30:8000/live/stream.flv',
-  'http://192.168.0.30:8000/live/stream2.flv',
-  'http://192.168.0.30:8000/live/stream.flv',
-  'http://192.168.0.30:8000/live/stream2.flv',
-  'http://192.168.0.30:8000/live/stream.flv',
-  'http://192.168.0.30:8000/live/stream2.flv',
-  
- 
+  'http://192.168.0.30:8000/show/session9/stream8.flv'
 ]
 
 
--- src/components/video/index.js
+++ src/components/video/index.js
@@ -4,6 +4,7 @@ import ReactPlayer from "react-player";
 export default function Video({ url }) {
   return (
     <>
+    <h2>Holla</h2>
       <ReactPlayer
         className="react-player"
         url={url}
